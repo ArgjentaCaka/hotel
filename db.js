b27d54a0992d2c783b2a9a6f6a915bd6355f5b11
ElzaGit <ek55329@ubt-uni.net>
2024-12-23 13:33:59 +0100

    AddMongoDB

---
+++ db.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+var mongoURL = 'mongodb+srv://elzakrasniqi2:Mongo123.@cluster0.mwxtd.mongodb.net/mern-rooms'
+
+mongoose.connect(mongoURL , {useUnifiedTopology : true , useNewUrlParser:true})
+
+var connection = mongoose.connection
+
+connection.on('error' , ()=>{
+    console.log('Mongo DB Connection failed')
+})
+
+connection.on('connected' , ()=>{
+    console.log('Mongo DB Connection Successful')
+})
+
+module.exports = mongoose
