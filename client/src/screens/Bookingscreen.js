7b0a1b32ca972cb366173dcb7c1c5b77b4999070
ElzaGit <ek55329@ubt-uni.net>
2025-01-03 22:00:51 +0100

    Added Details

--- client/src/screens/Bookingscreen.js
+++ client/src/screens/Bookingscreen.js
@@ -31,19 +31,45 @@ function Bookingscreen() {
     }, [roomid]);  // The effect will run whenever the roomid changes
     
     return (
-        <div>
+        <div className='m-5'>
             {loading ? (
                 <h1>Loading...</h1>
             ) : error ? (
                 <h1>Error...</h1>
             ) : (
                 <div>
-                    <div className="row">
+                    <div className="row justify-content-center mt-5 bs">
                         <div className="col-md-5">
                             <h1>{room.name}</h1>
                             {room.imageurls?.[0] && <img src={room.imageurls[0]} className="bigimg" />}
                         </div>
-                        <div className="col-md-5"></div>
+
+                        <div className="col-md-5" style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-end', marginTop: '50px' }}>
+                            <div style={{ textAlign: 'right' }}>
+                                <h1>Booking Details</h1>
+                                <hr />
+                                <b>
+                                    <p>Name :</p>
+                                    <p>From Date :</p>
+                                    <p>To Date :</p>
+                                    <p>Max Count : {room.maxCount}</p>
+                                </b>
+                            </div>
+
+                            <div style={{ textAlign: 'right', marginTop: '20px' }}>
+                                <b>
+                                    <h1>Amount</h1>
+                                    <hr />
+                                    <p>Total Days:</p>
+                                    <p>Rent Per Day : {room.rentperday}</p>
+                                    <p>Total Amount</p>
+                                </b>
+                            </div>
+
+                            <div style={{ float: 'right', marginTop: '20px' }}>
+                                <button className='btn btn-primary'>Pay Now</button>
+                            </div>
+                        </div>
                     </div>
                 </div>
             )}
